54cae4c22520c1e34fe5f6ab8de7a39e7e4a5aa6
Ahsn@1234 <ahmad507eb@gmail.com>
2023-07-22 19:10:01 +0500

    first commit

---
+++ lms.cs
@@ -0,0 +1,262 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+
+//namespace LibraryMngSystem
+//{
+//    class Book
+//    {
+//        public string Id { get; set; }
+//        public string Title { get; set; }
+//        public string Author { get; set; }
+//        public string Genre { get; set; }
+//        public int PublicationYear { get; set; }
+//        public bool IsBorrowed { get; set; }
+//        public string BorrowerName { get; set; }
+//    }
+
+//    class MyLibrary
+//    {
+//        private List<Book> books;
+
+//        public MyLibrary()
+//        {
+//            books = new List<Book>();
+//        }
+
+//        public void AddBook()
+//        {
+//            Console.WriteLine("Enter the title of book:");
+//            string title = Console.ReadLine();
+
+//            Console.WriteLine("Enter the author of book:");
+//            string author = Console.ReadLine();
+
+//            Console.WriteLine("Enter the genre of book:");
+//            string genre = Console.ReadLine();
+
+//            Console.WriteLine("Enter the publication year of book:");
+//            int publicationYear = int.Parse(Console.ReadLine());
+
+//            string id = Guid.NewGuid().ToString();
+
+//            Book book = new Book
+//            {
+//                Id = id,
+//                Title = title,
+//                Author = author,
+//                Genre = genre,
+//                PublicationYear = publicationYear,
+//                IsBorrowed = false,
+//                BorrowerName = ""
+//            };
+
+//            books.Add(book);
+
+//            SaveBooksToFile();
+//            Console.WriteLine("Book is added successfully.");
+//        }
+
+//        public void DisplayAllBooks()
+//        {
+//            if (books.Count == 0)
+//            {
+//                Console.WriteLine("No any book found in the library.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("MyLibrary books are:");
+
+//                foreach (var bk in books)
+//                {
+//                    Console.WriteLine($"Id is: {bk.Id}");
+//                    Console.WriteLine($"Title is: {bk.Title}");
+//                    Console.WriteLine($"Author is: {bk.Author}");
+//                    Console.WriteLine($"Genre is: {bk.Genre}");
+//                    Console.WriteLine($"Publication Year is: {bk.PublicationYear}");
+//                    Console.WriteLine($"Borrowed: {(bk.IsBorrowed ? "Yes" : "No")}");
+//                    Console.WriteLine($"Borrower Name is: {bk.BorrowerName}");
+//                    Console.WriteLine();
+//                }
+//            }
+//        }
+
+//        public void SearchBook()
+//        {
+//            Console.WriteLine("Enter the term for searching book:");
+//            string searchTerm = Console.ReadLine();
+
+//            List<Book> matchingBooks = books.FindAll(book =>
+//                book.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+//                book.Author.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+//                book.Genre.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
+
+//            if (matchingBooks.Count == 0)
+//            {
+//                Console.WriteLine("No any book found matching the search term.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Your matching books are:");
+
+//                foreach (var bk in matchingBooks)
+//                {
+//                    Console.WriteLine($"Id is: {bk.Id}");
+//                    Console.WriteLine($"Title is: {bk.Title}");
+//                    Console.WriteLine($"Author is: {bk.Author}");
+//                    Console.WriteLine($"Genre is: {bk.Genre}");
+//                    Console.WriteLine($"Publication Year is: {bk.PublicationYear}");
+//                    Console.WriteLine($"Borrowed: {(bk.IsBorrowed ? "Yes" : "No")}");
+//                    Console.WriteLine($"Borrower Name is: {bk.BorrowerName}");
+//                    Console.WriteLine();
+//                }
+//            }
+//        }
+
+//        public void BorrowBook()
+//        {
+//            Console.WriteLine("Enter the id of the book that you wanted to borrow from library:");
+//            string id = Console.ReadLine();
+
+//            Book book = books.Find(b => b.Id == id);
+
+//            if (book == null)
+//            {
+//                Console.WriteLine("No any book found with the your provided id.");
+//            }
+//            else if (book.IsBorrowed)
+//            {
+//                Console.WriteLine("The book is already borrowed by someone.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Enter your name:");
+//                string borrowerName = Console.ReadLine();
+
+//                book.IsBorrowed = true;
+//                book.BorrowerName = borrowerName;
+
+//                SaveBooksToFile();
+//                Console.WriteLine("Book is borrowed successfully.");
+//            }
+//        }
+
+//        public void ReturnBook()
+//        {
+//            Console.WriteLine("Enter the id of the book that you wanted to return back to library:");
+//            string id = Console.ReadLine();
+
+//            Book book = books.Find(b => b.Id == id);
+
+//            if (book == null)
+//            {
+//                Console.WriteLine("No any book found with the your provided id.");
+//            }
+//            else if (!book.IsBorrowed)
+//            {
+//                Console.WriteLine("The book is not currently borrowed.");
+//            }
+//            else
+//            {
+//                book.IsBorrowed = false;
+//                book.BorrowerName = "";
+
+//                SaveBooksToFile();
+//                Console.WriteLine("Book is returned successfully.");
+//            }
+//        }
+
+//        private void SaveBooksToFile()
+//        {
+//            using (StreamWriter writer = new StreamWriter("myLibraymanagmentsystem.txt"))
+//            {
+//                foreach (var bk in books)
+//                {
+//                    writer.WriteLine($"{bk.Id},{bk.Title},{bk.Author},{bk.Genre},{bk.PublicationYear},{bk.IsBorrowed},{bk.BorrowerName}");
+//                }
+//            }
+//        }
+
+//        public void LoadBooksFromFile()
+//        {
+//            if (File.Exists("myLibraymanagmentsystem.txt"))
+//            {
+//                using (StreamReader rd = new StreamReader("myLibraymanagmentsystem.txt"))
+//                {
+//                    string ln;
+
+//                    while ((ln = rd.ReadLine()) != null)
+//                    {
+//                        string[] parts = ln.Split(',');
+
+//                        Book book = new Book
+//                        {
+//                            Id = parts[0],
+//                            Title = parts[1],
+//                            Author = parts[2],
+//                            Genre = parts[3],
+//                            PublicationYear = int.Parse(parts[4]),
+//                            IsBorrowed = bool.Parse(parts[5]),
+//                            BorrowerName = parts[6]
+//                        };
+
+//                        books.Add(book);
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+//    class MyLMS
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyLibrary mylib = new MyLibrary();
+//            mylib.LoadBooksFromFile();
+
+//            bool exitProg = false;
+
+//            while (!exitProg)
+//            {
+//                Console.WriteLine("MyLibrary Management System");
+//                Console.WriteLine("1. Add a Book");
+//                Console.WriteLine("2. Display All Books");
+//                Console.WriteLine("3. Search for a Book");
+//                Console.WriteLine("4. Borrow a Book");
+//                Console.WriteLine("5. Return a Book");
+//                Console.WriteLine("6. Exit");
+//                Console.WriteLine("Enter your choice (1-6):");
+
+//                string yourChoice = Console.ReadLine();
+
+//                switch (yourChoice)
+//                {
+//                    case "1":
+//                        mylib.AddBook();
+//                        break;
+//                    case "2":
+//                        mylib.DisplayAllBooks();
+//                        break;
+//                    case "3":
+//                        mylib.SearchBook();
+//                        break;
+//                    case "4":
+//                        mylib.BorrowBook();
+//                        break;
+//                    case "5":
+//                        mylib.ReturnBook();
+//                        break;
+//                    case "6":
+//                        exitProg = true;
+//                        break;
+//                    default:
+//                        Console.WriteLine("Invalid choice, Try again once more");
+//                        break;
+//                }
+
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
+
---
+++ mylms.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Mng_System
+{
+    // DTO (Data Transfer Object) class to represent a Book
+    public class Book
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int PublicationYear { get; set; }
+        public bool IsBorrowed { get; set; }
+        public string Borrower { get; set; }
+    }
+
+    // DAL (Data Access Layer) class to handle file operations
+    public class LibraryDAL
+    {
+        private const string FilePath = "myLibrarymngtsystem.txt";
+
+        public List<Book> GetAllBooks()
+        {
+            List<Book> books = new List<Book>();
+
+            if (File.Exists(FilePath))
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(',');
+                    Book book = new Book
+                    {
+                        id = Convert.ToInt32(data[0]),
+                        Title = data[1],
+                        Author = data[2],
+                        Genre = data[3],
+                        PublicationYear = Convert.ToInt32(data[4]),
+                        IsBorrowed = Convert.ToBoolean(data[5]),
+                        Borrower = data[6]
+                    };
+                    books.Add(book);
+                }
+            }
+
+            return books;
+        }
+
+        public void SaveAllBooks(List<Book> books)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (Book book in books)
+            {
+                string line = $"{book.id},{book.Title},{book.Author},{book.Genre},{book.PublicationYear},{book.IsBorrowed},{book.Borrower}";
+                lines.Add(line);
+            }
+
+            File.WriteAllLines(FilePath, lines);
+        }
+    }
+
+    // BLL (Business Logic Layer) class to perform library operations
+    public class MyLibraryBLL
+    {
+        private List<Book> books;
+        private LibraryDAL libraryDAL;
+
+        public MyLibraryBLL()
+        {
+            libraryDAL = new LibraryDAL();
+            books = libraryDAL.GetAllBooks();
+        }
+
+        public void AddBook(string title, string author, string genre, int publicationYear)
+        {
+            int nextId = books.Count > 0 ? books.Max(b => b.id) + 1 : 1;
+
+            Book book = new Book
+            {
+                id = nextId,
+                Title = title,
+                Author = author,
+                Genre = genre,
+                PublicationYear = publicationYear,
+                IsBorrowed = false,
+                Borrower = string.Empty
+            };
+
+            books.Add(book);
+            libraryDAL.SaveAllBooks(books);
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        public List<Book> SearchBooks(string searchTerm)
+        {
+            searchTerm = searchTerm.ToLower();
+
+            return books.Where(b =>
+                b.Title.ToLower().Contains(searchTerm) ||
+                b.Author.ToLower().Contains(searchTerm) ||
+                b.Genre.ToLower().Contains(searchTerm)).ToList();
+        }
+
+        public void BorrowBook(int bookId, string borrower)
+        {
+            Book book = books.FirstOrDefault(b => b.id == bookId);
+
+            if (book != null && !book.IsBorrowed)
+            {
+                book.IsBorrowed = true;
+                book.Borrower = borrower;
+                libraryDAL.SaveAllBooks(books);
+            }
+        }
+
+        public void ReturnBook(int bookId)
+        {
+            Book book = books.FirstOrDefault(b => b.id == bookId);
+
+            if (book != null && book.IsBorrowed)
+            {
+                book.IsBorrowed = false;
+                book.Borrower = string.Empty;
+                libraryDAL.SaveAllBooks(books);
+            }
+        }
+    }
+
+    // Console application to interact with the library management system
+    public class Program
+    {
+        private static MyLibraryBLL libBLL = new MyLibraryBLL();
+
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the MyLibrary Management System!");
+
+            bool exitProgram = false;
+
+            while (!exitProgram)
+            {
+                Console.WriteLine("\nPlease select an option:");
+                Console.WriteLine("1. Add a Book");
+                Console.WriteLine("2. Display All Books");
+                Console.WriteLine("3. Search for a Book");
+                Console.WriteLine("4. Borrow a Book");
+                Console.WriteLine("5. Return a Book");
+                Console.WriteLine("6. Exit");
+
+                Console.Write("\nEnter your choice (1-6): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddBook();
+                        break;
+                    case "2":
+                        DisplayAllBooks();
+                        break;
+                    case "3":
+                        SearchBook();
+                        break;
+                    case "4":
+                        BorrowBook();
+                        break;
+                    case "5":
+                        ReturnBook();
+                        break;
+                    case "6":
+                        exitProgram = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Thank you for using the Library Management System!");
+        }
+
+        static void AddBook()
+        {
+            Console.WriteLine("\nAdding a Book");
+
+            Console.Write("Enter the book's title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Enter the book's author: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Enter the book's genre: ");
+            string genre = Console.ReadLine();
+
+            Console.Write("Enter the book's publication year: ");
+            int publicationYear = Convert.ToInt32(Console.ReadLine());
+
+            libBLL.AddBook(title, author, genre, publicationYear);
+
+            Console.WriteLine("Book added successfully!");
+        }
+
+        static void DisplayAllBooks()
+        {
+            Console.WriteLine("\nDisplaying All Books");
+
+            List<Book> books = libBLL.GetAllBooks();
+
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books found.");
+            }
+            else
+            {
+                Console.WriteLine("{0,-5} {1,-20} {2,-20} {3,-15} {4,-10}", "ID", "Title", "Author", "Genre", "Publication Year");
+                Console.WriteLine("--------------------------------------------------------------------------------");
+
+                foreach (Book book in books)
+                {
+                    Console.WriteLine("{0,-5} {1,-20} {2,-20} {3,-15} {4,-10}", book.id, book.Title, book.Author, book.Genre, book.PublicationYear);
+                }
+            }
+        }
+
+        static void SearchBook()
+        {
+            Console.WriteLine("\nSearch for a Book");
+
+            Console.Write("Enter the search term: ");
+            string searchTerm = Console.ReadLine();
+
+            List<Book> searchResults = libBLL.SearchBooks(searchTerm);
+
+            if (searchResults.Count == 0)
+            {
+                Console.WriteLine("No books found matching the search term.");
+            }
+            else
+            {
+                Console.WriteLine("{0,-5} {1,-20} {2,-20} {3,-15} {4,-10}", "ID", "Title", "Author", "Genre", "Publication Year");
+                Console.WriteLine("--------------------------------------------------------------------------------");
+
+                foreach (Book book in searchResults)
+                {
+                    Console.WriteLine("{0,-5} {1,-20} {2,-20} {3,-15} {4,-10}", book.id, book.Title, book.Author, book.Genre, book.PublicationYear);
+                }
+            }
+        }
+
+        static void BorrowBook()
+        {
+            Console.WriteLine("\nBorrow a Book");
+
+            Console.Write("Enter the book's ID: ");
+            int bookId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the borrower's name: ");
+            string borrower = Console.ReadLine();
+
+            libBLL.BorrowBook(bookId, borrower);
+
+            Console.WriteLine("Book borrowed successfully!");
+        }
+
+        static void ReturnBook()
+        {
+            Console.WriteLine("\nReturn a Book");
+
+            Console.Write("Enter the book's ID: ");
+            int bookId = Convert.ToInt32(Console.ReadLine());
+
+            libBLL.ReturnBook(bookId);
+
+            Console.WriteLine("Book returned successfully!");
+        }
+    }
+
+}
